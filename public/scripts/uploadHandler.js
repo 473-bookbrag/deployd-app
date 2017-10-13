1a946f5be28ff03bfc9199ea3b204c97f4d5ee0c
Kazander Antonio <kazander95@gmail.com>
2017-10-13 00:11:17 -0700

    Full book form working

--- public/scripts/uploadHandler.js
+++ public/scripts/uploadHandler.js
@@ -4,28 +4,54 @@
   var App = window.App || {};
   var $ = window.jQuery;
 
-  function UploadHandler(selector) {
 
+
+  function UploadHandler(formSelector, imageInputSelector, collectionUrl) {
+
+    this.collectionUrl = collectionUrl;
+    this.imageInputSelector = imageInputSelector;
     console.log("selected");
-    if (!selector) {
-      throw new Error('No selector provdied');
+    if (!formSelector) {
+      throw new Error('No formSelector provdied');
     }
 
-    this.$formElement = $(selector);
+    this.$formElement = $(formSelector);
     if (this.$formElement.length === 0) {
-      throw new Error('Could not find element with selector: ' + selector);
+      throw new Error('Could not find element with formSelector: ' + formSelector);
     }
   }
 
-  function sendFile(file) {
-    var formData = new FormData();
-    formData.append('uploadedFile', file);
+  function sendRemainder(formData){
+    'use strict';
+    console.log(formData);
     $.post({
-      url: "http://localhost:2403/imageupload",
-      contentType: false,
-      processData: false,
-      data: formData,
+      url: "http://localhost:2403/books",
+      contentType: "application/json",
+      data: JSON.stringify(formData),
     });
+  }
+
+  function sendData(formData, file) {
+    'use strict';
+    var imageData = new FormData();
+
+    imageData.append('uploadedFile', file);
+    if(file){
+      $.post({
+        url: "http://localhost:2403/imageupload",
+        processData: false,
+        contentType: false,
+        data: imageData,
+        success: function(val){
+            formData["imagename"] = val[0].filename;
+            console.log(formData.imagename);
+            sendRemainder(formData);
+        }
+      });
+    }
+    else{
+      sendRemainder(formData);
+    }
   }
 
   UploadHandler.prototype.addUploadHandler = function(fn) {
@@ -33,13 +59,19 @@
     this.$formElement.on('submit', function(event) {
       event.preventDefault();
 
+      var formData = {};
+      $(this).serializeArray().forEach(function(item) {
+       formData[item.name] = item.value;
+       console.log(item.name + ' is ' + item.value);
+      });
+
       console.log("does it get here");
 
 
 
-      var file = document.querySelector('#image-input').files[0];
+      var file = document.querySelector('#book-image-upload').files[0];
 
-      sendFile(file);
+      sendData(formData, file);
 
 
     });
