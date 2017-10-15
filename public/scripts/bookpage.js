165509db289aceeb9eca388aa9b17168d3df8bdb
Philip Chung <philipchung1995@yahoo.com>
2017-10-14 20:40:13 -0700

    Add Dan's ESLint config and autofix issues (indentation, quotes, etc.)

--- public/scripts/bookpage.js
+++ public/scripts/bookpage.js
@@ -1,5 +1,5 @@
 
 var App = window.App;
 var UploadHandler = App.UploadHandler;
-var uploadHandler = new UploadHandler("#book-upload", "#book-image-upload", "http://localhost:2403/books");
+var uploadHandler = new UploadHandler('#book-upload', '#book-image-upload', 'http://localhost:2403/books');
 uploadHandler.addUploadHandler(function(fn){});
--- public/scripts/bookshelfpage.js
+++ public/scripts/bookshelfpage.js
@@ -1,4 +1,4 @@
 var App = window.App;
 var UploadHandler = App.UploadHandler;
-var uploadHandler = new UploadHandler("#bookshelf-upload", "#bookshelf-image-upload", "http://localhost:2403/bookshelves");
+var uploadHandler = new UploadHandler('#bookshelf-upload', '#bookshelf-image-upload', 'http://localhost:2403/bookshelves');
 uploadHandler.addUploadHandler(function(fn){});
