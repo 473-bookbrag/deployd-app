4dc3114a53fadc9fe3b60a4e1605056752d6739e
Philip Chung <philipchung1995@yahoo.com>
2017-10-16 11:29:40 -0700

    Fix validation code (because the get event modifies the owner property)

--- resources/books/validate.js
+++ resources/books/validate.js
@@ -1,4 +1,4 @@
 cancelUnless(me, 'You must be logged in to add a book', 401);
 dpd.bookshelves.get(this.bookshelf, function(result, error) {
-    cancelUnless(result && isMe(result.owner), 'Cannot add a book to that bookshelf', 401);
+    cancelUnless(result && isMe(result.owner.id), 'Cannot add a book to that bookshelf', 401);
 });
