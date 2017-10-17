a53393362363812fd786b5ae3b580f43d22bee7a
shelleypham <phamshelley@csu.fullerton.edu>
2017-10-16 18:36:37 -0700

    these typos make me cry

--- public/scripts/followers.js
+++ public/scripts/followers.js
@@ -1,8 +1,8 @@
 dpd.users.me(function(r, e) {
     if (r) {
         var x = '';
-        for (var i in r.following) {
-            dpd.users.get(r.following[i], function(result) {
+        for (var i in r.followers) {
+            dpd.users.get(r.followers[i], function(result) {
                 var name = result.name;
                 x += '<span style=\"display:block\">' + name + '</span>';
                 $('#user-list2').html(x);
